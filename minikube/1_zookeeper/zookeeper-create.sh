54aa3a38dff9b30b54a8cfbb6d3121a675b507bd
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-11-27 15:31:13 +0100

    No more PodDisruptionBudgets for Minikube setups

--- minikube/1_zookeeper/zookeeper-create.sh
+++ minikube/1_zookeeper/zookeeper-create.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 kubectl apply -f 10_zookeeper-configmap.yml
-kubectl apply -f 11_zookeeper-poddisruptionbudget.yml
 kubectl apply -f 12_zookeeper-service.yml
 kubectl apply -f 13_zookeeper-statefulset.yml
